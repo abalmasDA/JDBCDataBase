740acfeee13de05070ee2719d8db877d6df4cbc5
abalmasDA <abalmasit@gmail.com>
2023-08-08 20:04:26 +0300

    Commit2

--- src/main/java/org/example/DataBaseConnection.java
+++ src/main/java/org/example/DataBaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataBaseConnection {
     private static final String url = "jdbc:mysql://localhost:3306/some_data_base";
     private static final String userName = "root";
-    private static final String password = "Dnepr19977";
+    private static final String password = "XXX";
 
     public static String getUrl() {
         return url;
